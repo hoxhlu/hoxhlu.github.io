bab1310b7893a86869be781d4bcb9014a46d686c
hoxhlu <90006596+hoxhlu@users.noreply.github.com>
2022-03-20 11:18:19 -0400

    Add files via upload

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 100, actionDuration:900, jumpHeight: 400, makeResponsive: true, numOfCollectables:9, removeCollectableAfterCollected: true, maxSimultaneousNPCs:1, npcSpeed: 180, collectableRadius: 20.8,});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 80, timeBetweenNPCs: 3200, npcCollisionRadius: 100, actionDuration:900, jumpHeight: 400, makeResponsive: true, numOfCollectables:9, removeCollectableAfterCollected: true, maxSimultaneousNPCs:1, npcSpeed: 180, collectableRadius: 30, pointsPerNPCInteracted: 0,playUntilScore: 9,removeNPCAfterInteractedDelay: 1600,postGameScreenImageURLs:["./img/post-screen.png"], preGameScreenImageURLs: ["./img/pre-screen.png"]});
 });
